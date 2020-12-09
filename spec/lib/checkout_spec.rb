b28cc8a5a31a5fbd0cba4d576f1a7d52f2224c32
Sergio Brocos <sergiobrocos@gmail.com>
2020-12-09 20:18:24 +0100

    the correct final test of checkout

--- spec/lib/checkout_spec.rb
+++ spec/lib/checkout_spec.rb
@@ -29,5 +29,58 @@ describe Checkout do
 
       it { expect(checkout.total).to eq 19.34 }
     end
+
+    context 'with Basket: GR1,SR1,GR1,GR1,CF1' do
+      before do
+        checkout.scan tea
+        checkout.scan strawberries
+        checkout.scan tea
+        checkout.scan tea
+        checkout.scan coffee
+      end
+
+      it 'returns total 22.45' do
+        expect(checkout.total).to eq 22.45
+      end
+    end
+
+    context 'with Basket:  GR1,GR1 ' do
+      before do
+        checkout.scan tea
+        checkout.scan tea
+      end
+
+      it 'returns total 22.45' do
+        expect(checkout.total).to eq 3.11
+      end
+    end
+
+    context 'with Basket: SR1,SR1,GR1,SR1' do
+      before do
+        checkout.scan strawberries
+        checkout.scan strawberries
+        checkout.scan tea
+        checkout.scan tea
+        checkout.scan strawberries
+      end
+
+      it 'returns total 22.45' do
+        expect(checkout.total).to eq 16.61
+      end
+    end
+
+    context 'with Basket: GR1,CF1,SR1,CF1,CF1' do
+      before do
+        checkout.scan tea
+        checkout.scan coffee
+        checkout.scan strawberries
+        checkout.scan coffee
+        checkout.scan coffee
+      end
+
+      it 'returns total 22.45' do
+        expect(checkout.total).to eq 30.57
+      end
+    end
   end
 end
